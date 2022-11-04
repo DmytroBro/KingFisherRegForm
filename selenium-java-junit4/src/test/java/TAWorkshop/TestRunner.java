f12de1bc266b83a8918fcc8f256144c888585a13
Dmytro Kovtun <dmytrobrovar@gmail.com>
2022-11-04 10:49:45 +0200

    POM file updated with new versions

--- selenium-java-junit4/src/test/java/TAWorkshop/TestRunner.java
+++ selenium-java-junit4/src/test/java/TAWorkshop/TestRunner.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
         features = "src/test/java/TAWorkshop/features",
         glue = "TAWorkshop.steps",
         tags = "@TestTag",
-        plugin = {"pretty", "html:target/cucumber-report.html"},
-        publish = true
+        plugin = {"pretty", "html:target/cucumber-report.html"}
+//        publish = true
 )
 
 public class TestRunner {
